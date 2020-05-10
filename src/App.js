70157926e444022b946759fe63e7d06bb0ae3c2d
Jason <jasylwong@gmail.com>
2020-05-10 21:11:28 +0100

    add search functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import './App.css';
+require('dotenv').config()
+
+const Recipe = ({ recipe }) => {
+  return (
+    <div key={recipe.label}>
+      <div >{recipe.recipe.label}</div>
+      <img src={recipe.recipe.image} />
+    </div>
+  )
+}
 
 function App() {
   const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('chicken')
   const [recipes, setRecipes] = useState([])
   
   const handleSearch = (event) => {
     setSearch(event.target.value)
   }
 
-  useEffect(() => {
-    axios.get()
-  })
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setQuery(search)    
+  }
+
+  const hook = () => {
+    axios.get(`https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_EDAMAME_ID}&app_key=${process.env.REACT_APP_EDAMAME_KEY}`).then(res => {
+      setRecipes(res.data.hits)
+    })
+  }
+
+  useEffect(hook, [query])
+
   return (
     <div className="App">
       <h1>Recip-easy</h1>
-      <input value={search} onChange={handleSearch}></input>
+      <form onSubmit={handleSubmit}>
+        <input value={search} onChange={handleSearch}></input>
+        <input type="submit" value="submit" />
+      </form>
       <br />
-      
+      {recipes.map(recipe => <Recipe key={recipe.recipe.label} recipe={recipe} /> )}
     </div>
   );
 }
