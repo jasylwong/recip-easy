f829e61709db988244074253378784cb3489bed7
Jason <jasylwong@gmail.com>
2020-05-10 19:20:51 +0100

    add search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('')
+  const [recipes, setRecipes] = useState([])
+  
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  useEffect(() => {
+    axios.get()
+  })
   return (
     <div className="App">
-      Hello
+      <h1>Recip-easy</h1>
+      <input value={search} onChange={handleSearch}></input>
+      <br />
+      
     </div>
   );
 }
