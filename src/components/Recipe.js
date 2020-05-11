c7b55261af6f4f889df6361c1cc5a9c6ec245c9b
Jason <jasylwong@gmail.com>
2020-05-11 08:44:22 +0100

    remove env variables

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Recipe = ({ recipe }) => {
   return (
-    <div key={recipe.label}>
-      <div >{recipe.recipe.label}</div>
-      <img src={recipe.recipe.image} />
+    <div key={recipe.label} className='recipe-container'>
+      <div className='recipe-label'>{recipe.recipe.label}</div>
+      <p className='recipe-image'><img src={recipe.recipe.image} /></p>
+      <div className='recipe-ingredients'>{recipe.recipe.ingredientLines}</div>
     </div>
   )
 }
